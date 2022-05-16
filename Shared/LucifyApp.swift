58dc4cf7829d837e9c2ce589ec1607e1f2cc5fde
Patrick Elfert <patrickelfert5@gmail.com>
2022-05-16 17:39:43 +0200

    Initial Commit

---
+++ Shared/LucifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LucifyApp.swift
+//  Shared
+//
+//  Created by Patrick Elfert on 16.05.22.
+//
+
+import SwiftUI
+
+@main
+struct LucifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
