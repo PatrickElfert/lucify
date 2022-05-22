839193c05a3c2872e60d3f69281fb94a7fa629e2
Patrick Elfert <patrickelfert5@gmail.com>
2022-05-22 18:40:20 +0200

    renaming, add alarm overview

--- Shared/AlarmView.swift
+++ Shared/AlarmView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct AlarmView: View {
+    var runningAlarms: [LDAlarm]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                ForEach(runningAlarms) {
+                    alarm in
+                    Text(alarm.date.toString(.custom("HH:mm"))!)
+                }
+            }.navigationTitle("Alarms")
+        }
     }
 }
 
 struct AlarmView_Previews: PreviewProvider {
     static var previews: some View {
-        AlarmView()
+        AlarmView(runningAlarms: [LDAlarm(fromNow: 8.hours)])
     }
 }
--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     
     var body: some View {
         TabView(selection: $selectedTab) {
-            AlarmView().tag("DreamDiary").tabItem {
+            AlarmEditView().tag("DreamDiary").tabItem {
                 Label("Alarms", systemImage: "alarm")
             }
         }
