6f0be3dbdf0ab017633d2d614bb008ec1dc91262
Patrick Elfert <patrickelfert5@gmail.com>
2022-05-22 13:09:10 +0200

    refactoring AlarmManager

--- Shared/AlarmManager.swift
+++ Shared/AlarmManager.swift
@@ -12,24 +12,19 @@ import AVFoundation
 class AlarmManager: ObservableObject {
     
     @Published var selectedMethod: Technique
-    @Published var techniquePreset: Preset
+    @Published var techniquePreset: TechniquePreset
     @Published var numberOfRausisAlarms: Int
     var runningAlarms: [LDAlarm] = []
+    var techniquePresets: [Technique:TechniquePreset]
     
     init() {
         selectedMethod = Technique.RAUSIS
         numberOfRausisAlarms = 2
-        let morningAlarmTime = Date.now.addingTimeInterval(60 * 60 * 9)
-        let wbtbAlarmTime = Date.now.addingTimeInterval(60 * 60 * 6)
         techniquePresets = [
-            Technique.RAUSIS:
-                Preset(morningAlarms: [LDAlarm(date: morningAlarmTime)], wbtbAlarms: [LDAlarm(date: wbtbAlarmTime)]),
-            Technique.FILD:
-                Preset(morningAlarms: [LDAlarm(date: morningAlarmTime)], wbtbAlarms: [LDAlarm(date: wbtbAlarmTime)]),
-            Technique.SSILD:
-                Preset(morningAlarms: [LDAlarm(date: morningAlarmTime)], wbtbAlarms: [LDAlarm(date: wbtbAlarmTime)]),
-            Technique.MILD:
-                Preset(morningAlarms: [LDAlarm(date: Date.now)], wbtbAlarms: [LDAlarm(date: wbtbAlarmTime)], isWbtbOptional: true, isWbtbVisible: false),
+            Technique.RAUSIS: TechniquePreset(),
+            Technique.FILD: TechniquePreset(),
+            Technique.SSILD: TechniquePreset(),
+            Technique.MILD: TechniquePreset(isWbtbOptional: true)
         ]
         techniquePreset = techniquePresets[Technique.RAUSIS]!
     }
@@ -41,11 +36,10 @@ class AlarmManager: ObservableObject {
     func setAlarms() throws -> Void {
         guard let url = Bundle.main.url(forResource: "scifi", withExtension: "mp3") else { return }
         for alarm in calculateAlarmsToCreate() {
-            let audioPlayer = try AVAudioPlayer(contentsOf: url)
+            let alarmToRun = LDAlarm(date: alarm.date, audioPlayer: try AVAudioPlayer(contentsOf: url))
+            guard let audioPlayer = alarmToRun.audioPlayer else { return }
             audioPlayer.prepareToPlay()
             audioPlayer.play(atTime: alarm.date.timeIntervalSinceNow)
-            var alarmToRun = LDAlarm(date: alarm.date)
-            alarmToRun.audioPlayer = audioPlayer
             runningAlarms.append(alarmToRun)
         }
     }
@@ -55,39 +49,59 @@ class AlarmManager: ObservableObject {
         if(selectedMethod == .RAUSIS) {
             let lastWbtbAlarm = techniquePreset.wbtbAlarms.last
             for i in 1...numberOfRausisAlarms {
-                alarmsToCreate.append(LDAlarm(date: (lastWbtbAlarm?.date.addingTimeInterval(TimeInterval(60 * i)))!))
+                alarmsToCreate.append(LDAlarm(date: (lastWbtbAlarm?.date.addingTimeInterval(2.minutes * Double(i)))!))
             }
         }
         return alarmsToCreate
     }
-    
-    var techniquePresets: [Technique:Preset]
 }
 
-
-
 struct LDAlarm: Identifiable {
-    init(date: Date) {
+    init(date: Date, audioPlayer: AVAudioPlayer? = nil) {
+        self.audioPlayer = audioPlayer
         self.date = date
+        self.id = UUID()
+    }
+    
+    init(fromNow: TimeInterval) {
+        self.date = Date.now.addingTimeInterval(fromNow)
         self.id = UUID()
     }
     var id: UUID
     var date: Date
-    var audioPlayer: AVAudioPlayer?
+    var audioPlayer: AVAudioPlayer?    
 }
 
-struct Preset {
-    init(morningAlarms: [LDAlarm] = [], wbtbAlarms: [LDAlarm] = [], isWbtbOptional: Bool = false, isWbtbVisible: Bool = true) {
+extension Int {
+    var hours: TimeInterval {
+        TimeInterval(self * 60 * 60)
+    }
+    var minutes: TimeInterval {
+        TimeInterval(self * 60)
+    }
+    var seconds: TimeInterval {
+        TimeInterval(self)
+    }
+}
+
+struct TechniquePreset {
+    init(morningAlarms: [LDAlarm] = [LDAlarm(fromNow: 9.hours)],
+         wbtbAlarms: [LDAlarm] = [LDAlarm(fromNow: 6.hours)],
+         repeatWbtb: Int = 0,
+         isWbtbOptional: Bool = false,
+         isWbtbVisible: Bool = true) {
         self.morningAlarms = morningAlarms
         self.wbtbAlarms = wbtbAlarms
         self.isWbtbOptional = isWbtbOptional
         self.isWbtbVisible = isWbtbVisible
+        self.repeatWbtb = repeatWbtb
     }
     
     var isWbtbOptional: Bool
     var isWbtbVisible: Bool
     var morningAlarms: [LDAlarm]
     var wbtbAlarms: [LDAlarm]
+    var repeatWbtb: Int
 }
 
 enum Technique: String, CaseIterable {
