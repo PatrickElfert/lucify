5455782e6f5e176a850c4954af6c287d6e179a03
Patrick Elfert <patrickelfert5@gmail.com>
2022-06-17 19:43:51 +0200

    change commit hook

--- pre-commit.bash
+++ pre-commit.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo “Running pre-commit hook”
-xcodebuild -quiet -project Lucify.xcodeproj -scheme “LucifyTests” -destination 'platform=iOS Simulator,name=iPhone XR,OS=15.4' test
+xcodebuild \
+  -project Lucify.xcodeproj \
+  -scheme "LucifyTests" \
+  -sdk iphonesimulator \
+  -destination 'platform=iOS Simulator,name=iPhone 12,OS=15.4' \
+  test
 if [ $? -ne 0 ]; then
  echo “Tests must pass before commit!”
  exit 1
